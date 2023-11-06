715d8ff88ebbc13c8f04cf3857a619d2a29893ac
Felipe Benoit <benoit@Felipes-MacBook-Pro.local>
2023-11-06 16:50:35 -0500

    time block colors past, present, future

--- Assets/script.js
+++ Assets/script.js
@@ -24,7 +24,16 @@ $(function () {
   var currentHour = dayjs().hour();
   $(".time-block").each(function () {
     var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
-    
+    if (timeBlockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+  
+
 
 
   // TODO: Add a listener for click events on the save button. This code should
